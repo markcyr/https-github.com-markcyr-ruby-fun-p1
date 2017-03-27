2fee4dc67144e1049e883da654d53035b542c048
markcyr <markcyr@msn.com>
2017-03-27 14:58:33 -0400

    exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -2,22 +2,31 @@ puts "Let's get out of the house and go for a walk or run"
 puts "If you say walk I will add 1 km"
 puts "If you say run I will add 5 km"
 puts "When you're finished walking say go home"
+puts "When you eat something say eat"
 distance = 0
-
+energy = 4
 puts "LET'S GO"
 puts " "
 puts " "
-puts "did you walk, run, or go home"
+puts "Did you walk, run, eat, or go home"
 user_input = gets.chomp
   until user_input == "go home"
   if user_input == "walk"
     distance += 1
-      puts "keep going"
-  else user_input == "run"
+    energy += 1
+      puts "KEEP GOING"
+  elsif user_input == "run"
     distance += 5
+    energy -= 1
+    puts "KEEP GOING"
+  elsif user_input == "eat"
+    energy += 1
+  else
+  puts "You need to answer the question"
  end
- puts "did you walk, run, or go home"
+ print "You're energy level is #{energy}, if it's below 2 you might want to eat something"
+ puts "Did you walk, run, eat, or go home"
 user_input = gets.chomp
 end
-  puts "Distance from home is #{distance}km's"
+  puts "Distance from home is #{distance}km's, and you're energy level is #{energy}."
   puts "GREAT JOB"
